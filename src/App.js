3c5963f73b0d46228f983ec14bf59720360c1ee8
Ansaf Muhammed <anchu56@gmail.com>
2022-01-18 07:51:52 +0530

    Objects placed as array and use those objects using Map function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import {useState} from 'react'
 import Counter from './Counter';
+import Employee from './Employee';
 function App() {
   const[count,setCount] = useState(0)
   const addCount=()=>{
   setCount(count+1)
   console.log(count);
   }
-  let obj = {
-    title : '1st Counter : ',
-    count,
-    place : 'Kannur'
-  }
+ let emp = [{name:'Ansaf',age:21},
+ {name:'Vasif',age:24},
+ {name:'Asif',age:26},
+ ]
   return (
     <div className="App" >
       <button onClick={addCount}>Add</button>
-      <Counter {...obj} />
+      <Counter title='1st Counter :' count={count} />
       <Counter title='2nd Counter :' count={count} />
+       
+
+    {
+      emp.map((obj,index)=>{
+        return(
+          <Employee key={index} name={obj.name} age={obj.age}  />
+        )
+      })
+    }
     </div>
     );
 }
