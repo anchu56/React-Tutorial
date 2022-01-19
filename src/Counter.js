7f3697a05577fca620070184a6c3b0d61bf599b0
Ansaf Muhammed <anchu56@gmail.com>
2022-01-19 09:18:20 +0530

    How return works in useEffect when we pass state as array's argument

--- src/Counter.js
+++ src/Counter.js
@@ -5,7 +5,9 @@ function Counter() {
     useEffect(()=>{
         console.log("Mounting"); // works when new component is being loaded
         console.log("Updating " +count);
-        
+        return()=>{
+            console.log("Clean up " +count); // If we give a state as array's argument then return is called first while updating.
+        }
 
     },[count]) // The variable given in the array is - Whichever obj is updated using useState, for whom useEffect has to be called   
     return (
