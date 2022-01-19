755686c87e0bacb06813a2cbd5f8e4845c8c2711
Ansaf Muhammed <anchu56@gmail.com>
2022-01-19 09:31:50 +0530

    passing 2 objects as argument of useEffect

--- src/Counter.js
+++ src/Counter.js
@@ -2,18 +2,21 @@ import React,{useState,useEffect} from 'react'
 
 function Counter() {
     const[count,setCount]=useState(0)
+    const[count2,setCount2]=useState(0)
     useEffect(()=>{
         console.log("Mounting"); // works when new component is being loaded
-        console.log("Updating " +count);
-        return()=>{
-            console.log("Clean up " +count); // If we give a state as array's argument then return is called first while updating.
-        }
+        console.log("Count1 " +count);
+        console.log("Count2 " +count2);
+        
 
-    },[count]) // The variable given in the array is - Whichever obj is updated using useState, for whom useEffect has to be called   
+    },[count,count2]) // The variable given in the array is - Whichever obj is updated using useState, for whom useEffect has to be called   
     return (
         <div>
             <button onClick={()=>setCount(count+1)}>Increment</button>
-            <h1>Hello I am Component : {count} </h1>
+            <h1>Hello I am Component 1 : {count} </h1>
+
+            <button onClick={()=>setCount2(count2+1)}>Increment</button>
+            <h1>Hello I am Component 2 : {count2} </h1>
         </div>
     )
 }
